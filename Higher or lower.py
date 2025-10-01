e9e4dc617466a6c261ef1c14d62a4718da8552fc
KARHU <vili.karhu@metropolia>
2025-10-01 10:10:54 +0300

    testii

--- Higher or lower.py
+++ Higher or lower.py
@@ -1,1 +1,21 @@
-print("Absolute cinema")
+import mysql.connector
+from geopy.distance import geodesic
+
+yhteys = mysql.connector.connect(
+    host='127.0.0.1',
+    port=3306,
+    database='flight_game',
+    user='root',
+    password='Python',
+    autocommit=True
+)
+
+cursor = yhteys.cursor()
+
+def hae_koordinaatit(icao):
+    sql = "SELECT latitude_deg, longitude_deg FROM airport WHERE ident = %s"
+    cursor.execute(sql, (icao,))
+    tulos = cursor.fetchone()
+    if tulos:
+        return (float(tulos[0]), float(tulos[1]))
+    return None
